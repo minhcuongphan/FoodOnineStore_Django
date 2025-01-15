8e9bce4a7a8dbb2e614c91d325d0cf656a6b3209
duocdd <dangduoc.itcnew@gmail.com>
2025-01-15 21:54:07 +0700

    Implemented the logic for approving vendor registrations

--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
+from vendor.models import Vendor
 
 # Restrcit the vendor from accessing the customer page
 def check_role_vendor(user):
